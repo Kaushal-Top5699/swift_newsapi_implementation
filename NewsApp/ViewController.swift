5d0dccb1dbc18f4be4458ea468b40401072aaee8
Kaushal <kaushaltop@me.com>
2021-05-25 17:36:34 +0530

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Kaushal Topinkatti B on 25/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
